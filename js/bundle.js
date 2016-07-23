fdd3aef892fc299bad96b7a32ade662ec3111c44
John Goodman <johncurtisgoodman@gmail.com>
2016-07-23 04:02:34 -0400

    Fix params bug in main input

--- js/bundle.js
+++ js/bundle.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   var input = document.getElementById('main-input');
-  input.onkeydown = function (e) {
+  input.onkeydown = function (event) {
     if (event.keyCode === 13) {
       console.log(input.value);
       input.value = '';
