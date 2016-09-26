7ce7aece4547384a72ed8795d1dcf744aab462ce
John Goodman <johncurtisgoodman@gmail.com>
2016-09-25 23:28:18 -0400

    Edit copy

--- js/areas/end.js
+++ js/areas/end.js
@@ -5,14 +5,14 @@ var Item = require('../item.js');
 var Exit = require('../exit.js');
 
 area = new Area ({
-  description: "A vast empty white space. This is the end of the demo.",
+  description: "A vast empty white space stretching outward into boundless infinity. This is the end of the demo,",
   name: 'end',
   worldMap: this,
   contents: [
     new Exit ({
       name: "door",
       checkText: "A heavy wooden door with a coat of chipped grey paint.",
-      description: "The door is behind you.",
+      description: "the door's behind you.",
       destinationName: 'wheatfield',
       verbs: ["check", "go to"],
     }),
--- js/wendigo.js
+++ js/wendigo.js
@@ -1243,14 +1243,14 @@
 	var Exit = __webpack_require__(7);
 	
 	area = new Area ({
-	  description: "A vast empty white space. This is the end of the demo.",
+	  description: "A vast empty white space stretching outward into boundless infinity. This is the end of the demo,",
 	  name: 'end',
 	  worldMap: this,
 	  contents: [
 	    new Exit ({
 	      name: "door",
 	      checkText: "A heavy wooden door with a coat of chipped grey paint.",
-	      description: "The door is behind you.",
+	      description: "the door's behind you.",
 	      destinationName: 'wheatfield',
 	      verbs: ["check", "go to"],
 	    }),
